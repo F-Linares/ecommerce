de5f683bd82790aeeb3d3e8804699838892091da
Fernando Linares <fernando.linares1991@gmail.com>
2021-11-29 06:10:45 -0600

    update

--- ecommerce/store/tests/test_models.py
+++ ecommerce/store/tests/test_models.py
@@ -7,9 +7,9 @@ class TestCategoriesModel(TestCase):
     def setUp(self):
         self.data1 = Category.objects.create(name= 'django', slug= 'django')
 
-        def test_category_model_entry(self):
-            """
-            Test Category model data insertion/types/field attributes
-            """
-            data = self.data1
-            self.assertTrue(isinstance(data, Category))
+    def test_category_model_entry(self):
+        """
+        Test Category model data insertion/types/field attributes
+        """
+        data = self.data1
+        self.assertTrue(isinstance(data, Category))
