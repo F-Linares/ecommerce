44b489fcbf097b0c9d10c4e5707d387c7543af23
Fernando Linares <fernando.linares1991@gmail.com>
2021-12-05 06:45:42 -0600

    Product single page view

--- ecommerce/store/urls.py
+++ ecommerce/store/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 app_name = 'store'
 
-urlpatterns = [   
+urlpatterns = [
     path('', views.all_products, name='all_products'),
     path('item/<slug:slug>/', views.product_detail, name='product_detail'),
 ]
