140b88d2b391cd06ea65e7f363250b00f57a4e0e
Fernando Linares <fernando.linares1991@gmail.com>
2021-12-07 04:51:14 -0600

    added gitignore and edits

--- ecommerce/store/tests/test_views.py
+++ ecommerce/store/tests/test_views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from django.http import HttpRequest
 from django.test import Client, RequestFactory, TestCase
 from django.urls import reverse
+from ecommerce.store.views import product_all
 
 from store.models import Category, Product
 from store.views import all_products
